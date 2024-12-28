b735482e1a47bd4752c293a4093278ad82a86847
KONO-anupam <anupam.nitrkl@gmail.com>
2024-12-28 13:06:28 +0530

    added variables to store the score of player and computer

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let opp='';
- 
+let playerScore=0;
+let compScore=0;
+
+const stone=document.createElement("button");
+const paper=document.createElement("button");
+const scissor=document.createElement("button");
+const resultt=document.createElement("div");
+document.body.appendChild(stone);
+document.body.appendChild(paper);
+document.body.appendChild(scissor);
+document.body.appendChild(resultt);
+
+
+
+
+stone.addEventListener("click",()=>{playRound("stone")})
+paper.addEventListener("click",()=>{playRound("paper")})
+scissor.addEventListener("click",()=>{playRound("scissor")})
+
     function getCompChoice(){
-        let cc=math.random();
+        let cc=Math.random();
         if(cc<=0.33){
             return'stone';
           
@@ -14,22 +32,40 @@ let opp='';
      } 
     }
 
-    function playRound(){
+    function playRound(playerChoice){
         opp = getCompChoice();
         const compChoice=document.createElement("p");
          compChoice.textContent=`The opponent chose ${opp}`
-         if(opp==='paper')
+         resultt.appendChild(compChoice);
+         if(opp==='paper'&& playerChoice==='scissor'){
+            resultt.textContent= 'u win';
+            playerScore++;
 
-    }
- 
+         }
+         else if(opp==='scissor'&& playerChoice==='stone'){
+            resultt.textContent= 'u win';
+            playerScore++;
+         }
+         else if(opp==='stone'&& playerChoice==='paper'){
+            resultt.textContent= 'u win';
+            playerScore++;
+         }
+        else if(opp==='stone'&& playerChoice==='stone'){
+            resultt.textContent = 'tie';
+        }
+        else if(opp==='paper'&& playerChoice==='paper'){
+            resultt.textContent= 'tie';
+        }
+        else if(opp==='scissor'&& playerChoice==='scissor'){
+            resultt.textContent= 'tie';
+        }
+        else{
+            resultt.textContent= 'u lose';
+            compScore++;
+        }
+     
 
+    }} 
 
- const stone=document.createElement("button");
- const paper=document.createElement("button");
- const scissor=document.createElement("button");
- stone.addEventListener("click",()=>{playRound(stone)})
- paper.addEventListener("click",()=>{playRound(paper)})
- scissor.addEventListener("click",()=>{playRound(scissor)})
- const resultt=document.createElement("div");
 
 
